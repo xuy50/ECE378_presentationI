d2555e41ab17d5f330b3547d8e82a2aad46e2173
xuy50 <78167288+xuy50@users.noreply.github.com>
2021-03-15 20:34:27 -0400

    Add files via upload

--- lib/lcdpcf8574/lcdpcf8574.h
+++ lib/lcdpcf8574/lcdpcf8574.h
@@ -27,8 +27,8 @@ References:
  *  @name  Definitions for Display Size 
  *  Change these definitions to adapt setting to your display
  */
-#define LCD_LINES           4     /**< number of visible lines of the display */
-#define LCD_DISP_LENGTH    40     /**< visibles characters per line of the display */
+#define LCD_LINES           2     /**< number of visible lines of the display */
+#define LCD_DISP_LENGTH    16     /**< visibles characters per line of the display */
 #define LCD_LINE_LENGTH  0x40     /**< internal line length of the display    */
 #define LCD_START_LINE1  0x00     /**< DDRAM address of first char of line 1 */
 #define LCD_START_LINE2  0x40     /**< DDRAM address of first char of line 2 */
